83f4d34fc096194350abbe92de8629b632308ee9
Fabio Petito <fabio@example.com>
2025-12-03 10:23:51 +0000

    Linked CSS to Theme

---
+++ src/fluent/generated/m2m_sp_theme_css_include_7d17345693edbe183321f2cbdd03d676.now.ts
@@ -0,0 +1,10 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['7d17345693edbe183321f2cbdd03d676'],
+    table: 'm2m_sp_theme_css_include',
+    data: {
+        sp_css_include: 'f212dcba5f8843d786299e168cb28371',
+        sp_theme: '7ba139dce64c477abec81af8d4d142e6',
+    },
+})
